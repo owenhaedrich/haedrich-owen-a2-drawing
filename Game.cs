e330d6a0249bcf55b03117be2a7cab3d362dea90
haedrichowen <98614633+haedrichowen@users.noreply.github.com>
2025-01-26 09:24:37 -0500

    init

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Linq;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -10,15 +11,16 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        // Place your variables here:
+        Vector2[] bugArray = [new Vector2(0,0)];
+        int bugSize = 3;
+        int bugSpeed = 1;
 
-
-        /// <summary>
-        ///     Setup runs once before the game loop begins.
-        /// </summary>
+        Vector2 platePosition = new Vector2(200, 200);
+        int plateSize = 10;
         public void Setup()
         {
-
+            Window.SetSize(400, 400); // Size is in pixels and ordered as (width, height)
+            Window.TargetFPS = 60; // Aim to render 60 times a second
         }
 
         /// <summary>
@@ -26,8 +28,23 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.OffWhite); // Reset the background
+
+            for (int i = 0; i < bugArray.Length; i++)
+            {
+                Vector2 bugPosition = bugArray[i];
+                Vector2 bugVector = Vector2.Normalize(platePosition - bugPosition) * bugSpeed;
+                Draw.FillColor = Color.Black;
+                Draw.Circle(bugPosition, bugSize);
+                bugArray[i] = bugPosition + bugVector;
+            }
+        }
+
+        public void spawnBug()
+        {
 
         }
+
     }
 
 }
