f315d3ee6894bd994841b9bae2d13c126ebb9c8d
haedrichowen <98614633+haedrichowen@users.noreply.github.com>
2025-01-26 19:42:17 -0500

    draw ant

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Drawing;
 using System.Linq;
 using System.Numerics;
 
@@ -11,8 +12,7 @@ namespace MohawkGame2D
     /// </summary>
     public class Game
     {
-        Vector2[] bugArray = [new Vector2(0,0)];
-        int bugSize = 3;
+        Vector2[] bugArray = [new Vector2(0,0), new Vector2(0, 200), new Vector2(0, 400)];
         int bugSpeed = 1;
 
         Vector2 platePosition = new Vector2(200, 200);
@@ -33,16 +33,50 @@ namespace MohawkGame2D
             for (int i = 0; i < bugArray.Length; i++)
             {
                 Vector2 bugPosition = bugArray[i];
-                Vector2 bugVector = Vector2.Normalize(platePosition - bugPosition) * bugSpeed;
-                Draw.FillColor = Color.Black;
-                Draw.Circle(bugPosition, bugSize);
-                bugArray[i] = bugPosition + bugVector;
+                Vector2 bugDirection = Vector2.Normalize(platePosition - bugPosition);
+                drawBug(bugPosition, bugDirection);
+                bugArray[i] = bugPosition + bugDirection * bugSpeed;
             }
         }
 
-        public void spawnBug()
+        public void drawBug(Vector2 position, Vector2 direction)
         {
+            var bugScale = 5;
+            
+            Draw.FillColor = Color.Black;
 
+            //Draw abdomen
+            Draw.Ellipse(position + (Vector2.UnitX * 5 * bugScale), new Vector2(10, 6) * bugScale);
+
+            //Draw thorax
+            Draw.Ellipse(position + (Vector2.UnitX * 14 * bugScale), new Vector2(12, 2) * bugScale);
+
+            //Draw head
+            Draw.Circle(position + (Vector2.UnitX * 20 * bugScale), 3 * bugScale);
+
+            //Draw antennae
+            Draw.Line(position + (Vector2.UnitX * 20 * bugScale), position + new Vector2(28, 3) * bugScale);
+            Draw.Line(position + (Vector2.UnitX * 20 * bugScale), position + new Vector2(28, -3) * bugScale);
+            Draw.Line(position + new Vector2(28, 3) * bugScale, position + new Vector2(30, 3) * bugScale);
+            Draw.Line(position + new Vector2(28, -3) * bugScale, position + new Vector2(30, -3) * bugScale);
+
+            //Draw front legs
+            Draw.Line(position + (Vector2.UnitX * 14 * bugScale), position + new Vector2(26, 7) * bugScale);
+            Draw.Line(position + (Vector2.UnitX * 14 * bugScale), position + new Vector2(26, -7) * bugScale);
+
+            //Draw middle legs
+            Draw.Line(position + (Vector2.UnitX * 14 * bugScale), position + new Vector2(14, 3) * bugScale);
+            Draw.Line(position + (Vector2.UnitX * 14 * bugScale), position + new Vector2(14, -3) * bugScale);
+            Draw.Line(position + new Vector2(14, 3) * bugScale, position + new Vector2(16, 5) * bugScale);
+            Draw.Line(position + new Vector2(14, -3) * bugScale, position + new Vector2(16, -5) * bugScale);
+            Draw.Line(position + new Vector2(16, 5) * bugScale, position + new Vector2(12, 9) * bugScale);
+            Draw.Line(position + new Vector2(16, -5) * bugScale, position + new Vector2(12, -9) * bugScale);
+
+            //Draw back legs
+            Draw.Line(position + (Vector2.UnitX * 10 * bugScale), position + new Vector2(9, 3) * bugScale);
+            Draw.Line(position + (Vector2.UnitX * 10 * bugScale), position + new Vector2(9, -3) * bugScale);
+            Draw.Line(position + new Vector2(9, 3) * bugScale, position + new Vector2(0, 7) * bugScale);
+            Draw.Line(position + new Vector2(9, -3) * bugScale, position + new Vector2(0, -7) * bugScale);
         }
 
     }
